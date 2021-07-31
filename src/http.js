8b827982c13aa81fefcd13568e7e8a0885e1b76d
Aditya Kansara <adityakan786@gmail.com>
2021-07-31 17:05:09 +0530

    first

---
+++ src/http.js
@@ -0,0 +1,59 @@
+class EasyHTTP {
+    // Make an HTTP GET request
+    async get(url) {
+
+        const res = await fetch(url);
+        const data = await res.json();
+        return data;
+    }
+
+    // Make a POST request
+    async post(url, data) {
+        const res = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        });
+        const user = await res.json();
+        console.log('New POST');
+        return user;
+    }
+
+    // Make an HTTP PUT request
+    async put(url, data) {
+
+        const res = await fetch(url, {
+            method: 'PUT',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        });
+
+        const user = await res.json();
+        console.log('POST updated');
+        return user;
+    }
+
+    // Make an HTTP DELETE request
+    async delete(url) {
+
+        const res = await fetch(url, {
+            method: 'DELETE',
+            headers: {
+                'content-type': 'application/json'
+            }
+        });
+        const user = await res.json();
+        console.log(user);
+        console.log('Post DELETED');
+        const res2 = await fetch(url);
+        const data = await res2.json();
+        return data;
+    }
+
+}
+
+export const http = new EasyHTTP();
